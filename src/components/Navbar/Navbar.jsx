b61b98504d4c0aa83bce4a0503d284f2512ad2b0
BlackHawk1303 <miguel.farias1303@gmail.com>
2023-11-19 22:53:59 -0300

    Refactorizaciones varias

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,6 @@
 import classes from './Navbar.module.css'
 import CartWidget from "../CartWidget/CartWidget"
 import { NavLink, useNavigate } from 'react-router-dom'
-
-//import {coderhouseLogo} from "./assets/logo-coderhouse.png"
-
 import coderhouseLogo from "./assets/logo-coderhouse.png";
 
 const Navbar = () => {
@@ -12,7 +9,6 @@ const Navbar = () => {
     return (
             <nav className={classes.container}>
             <h1 onClick={() => navigate('/')}><img className="logo" src={coderhouseLogo} alt="" /> Ecommerce</h1>
-            {/* <h1 onClick={() => navigate('/')}><img className="logo" src="./src/assets/logo-coderhouse.png" alt="" /> Ecommerce</h1> */}
             <section className={classes.buttonSection}>
                 <NavLink to='/category/proyectos' className={classes.button}>Proyectos</NavLink>
                 <NavLink to='/category/servicios' className={classes.button}>Servicios</NavLink>
